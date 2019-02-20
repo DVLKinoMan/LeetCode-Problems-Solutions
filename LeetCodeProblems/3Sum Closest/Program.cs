89e9ed84e12f2039ff3c747f8f3491767f2bd215
davit dvali <dvali.daviti@gmail.com>
2019-02-20 20:32:58 +0400

    Longest Common Prefix and Closes 3Sum

---
+++ LeetCodeProblems/3Sum Closest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Sum_Closest
+{
+    class Program
+    {
+        public static int ThreeSumClosest(int[] nums, int target)
+        {
+            if (nums.Length < 3)
+                return 0;
+
+            Array.Sort(nums);
+            int result = nums.Take(3).Sum();
+            for(int i=0; i<nums.Length; i++)
+            {
+                int begin = i + 1, end = nums.Length - 1;
+                while (begin < end)
+                {
+                    int sum = nums[i] + nums[begin] + nums[end];
+                    if (Math.Abs(result - target) > Math.Abs(sum - target))
+                        result = sum;
+
+                    if (result == target)
+                        return result;
+
+                    if (sum < target)
+                        begin++;
+                    else
+                        end--;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            ThreeSumClosest(new int[] { -1, 0, 1, 1, 55}, 3);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LeetCodeProblems/Longest Common Prefix/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Common_Prefix
+{
+    class Program
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return string.Empty;
+
+            int index = 0;
+            StringBuilder result = new StringBuilder();
+            while (true)
+            {
+                if (strs[0].Length - 1 < index)
+                    return result.ToString();
+
+                char ch = strs[0][index];
+                for (int i = 1; i < strs.Length; i++)
+                    if (strs[i].Length - 1 < index || strs[i][index] != ch)
+                        return result.ToString();
+                result.Append(ch);
+                index++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
