a805b54952d27a5d7650ab21a00585424e2dc0ee
davit dvali <dvali.daviti@gmail.com>
2019-02-22 20:39:29 +0400

    Valid Parentheses and Remove Nth Node from Linked list

---
+++ LeetCodeProblems/Remove Nth Node From End of List/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Nth_Node_From_End_of_List
+{
+    class Program
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var nodes = new List<ListNode>();
+            var currNode = head;
+            while (currNode != null)
+            {
+                nodes.Add(currNode);
+                currNode = currNode.next;
+            }
+            if (nodes.Count - n - 1 >= 0)
+                nodes.ElementAt(nodes.Count - n - 1).next = n != 1 ? nodes.ElementAt(nodes.Count - (n - 1)) : null;
+            else
+                return nodes.Count - (n - 1) >= nodes.Count ? null : nodes.ElementAt(nodes.Count - (n - 1));
+
+            return head;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LeetCodeProblems/Valid Parentheses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parentheses
+{
+    class Program
+    {
+        public static bool IsValid(string s)
+        {
+            var dic = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '{', '}' },
+                { '[', ']' }
+            };
+
+            var openBrackets = new List<char>();
+            foreach(var ch in s)
+            {
+                if (!dic.ContainsKey(ch))
+                {
+                    if (openBrackets.Count == 0 || dic[openBrackets.Last()] != ch)
+                        return false;
+                    else openBrackets.RemoveAt(openBrackets.Count - 1);
+                }
+                else openBrackets.Add(ch);
+            }
+
+            return openBrackets.Count == 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("[([]])"));
+            Console.ReadKey();
+        }
+    }
+}
