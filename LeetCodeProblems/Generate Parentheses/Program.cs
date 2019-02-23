06c0b0a2ea9a86f801d2f8fa1be699bfb38a2e42
davit dvali <dvali.daviti@gmail.com>
2019-02-23 17:01:35 +0400

    Merge Two Sorted Lists and Generate Parenthesis

---
+++ LeetCodeProblems/Generate Parentheses/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate_Parentheses
+{
+    class Program
+    {
+        //Logic is not right
+        //With IEnumarable<string>
+        //public static IEnumerable<string> GenerateParenthesis(int n)
+        //{
+        //    if (n == 1)
+        //    {
+        //        yield return "()";
+        //        yield break;
+        //    }
+
+        //    foreach(var str in GenerateParenthesis(n-1))
+        //    {
+        //        yield return string.Format("({0})", str);
+        //        var str1 = string.Format("(){0}", str);
+        //        var str2 = string.Format("{0}()", str);
+        //        yield return str1;
+        //        if (str1 != str2)
+        //            yield return str2;
+        //    }
+        //}
+
+        //Logic is not right
+        //public static IList<string> GenerateParenthesis(int n)
+        //{
+        //    IList<string> list;
+        //    if (n == 1)
+        //    {
+        //        list = new List<string> { "()" };
+        //        return list;
+        //    }
+
+        //    list = new List<string>();
+        //    var set = new HashSet<string>();
+        //    foreach (var str in GenerateParenthesis(n - 1))
+        //    {
+        //        set.Add(string.Format("({0})", str));
+        //        set.Add(string.Format("(){0}", str));
+        //        set.Add(string.Format("{0}()", str));
+        //    }
+
+
+
+        //    list = set.ToList();
+        //    return list;
+        //}
+
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> list = GenerateParenthesisRek(n).ToList();
+            return list;
+        }
+
+        public static IEnumerable<string> GenerateParenthesisRek(int n)
+        {
+            if(n==0)
+            {
+                yield return "";
+                yield break;
+            }
+
+            for (int c = 0; c < n; c++)
+                foreach (var left in GenerateParenthesisRek(c))
+                    foreach (var right in GenerateParenthesisRek(n - 1 - c))
+                        yield return string.Format("({0}){1}", left, right);
+        }
+
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (true)
+            {
+                Console.Write( string.Format("{0}) n = ", index));
+                int n = int.Parse(Console.ReadLine());
+                foreach (var str in GenerateParenthesis(n))
+                    Console.WriteLine(str);
+                index++;
+            }
+        }
+    }
+}
---
+++ LeetCodeProblems/Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Two_Sorted_Lists
+{
+    class Program
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null && l2 == null)
+                return null;
+
+            var currLeftNode = l1;
+            var currRigthNode = l2;
+            ListNode head;
+
+            if (currLeftNode != null && (currRigthNode == null || currLeftNode.val < currRigthNode.val))
+            {
+                head = currLeftNode;
+                currLeftNode = head.next;
+            }
+            else
+            {
+                head = currRigthNode;
+                currRigthNode = head.next;
+            }
+
+            ListNode currNode;
+            currNode = head;
+            while (currLeftNode!=null || currRigthNode != null)
+            {
+                if (currLeftNode != null && (currRigthNode == null || currLeftNode.val < currRigthNode.val))
+                {
+                    currNode.next = currLeftNode;
+                    currLeftNode = currLeftNode.next;
+                }
+                else
+                {
+                    currNode.next = currRigthNode;
+                    currRigthNode = currRigthNode.next;
+                }
+                currNode = currNode.next;
+            }
+
+            return head;
+        }
+
+        static void Main(string[] args)
+        {
+            MergeTwoLists(
+                new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = null
+                        }
+                    }
+                },
+                new ListNode(1)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = null
+                        }
+                    }
+                });
+
+        }
+    }
+}
